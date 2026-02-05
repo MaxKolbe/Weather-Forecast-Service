1e80d7183af61e5416884bffd785b2ac2e5b278c
Maxmillian <maximilianogbuabor@gmail.com>
2026-02-05 15:51:40 +0100

    Add dashboard routes and controllers

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -4,6 +4,7 @@ import { Request, Response, NextFunction } from "express";
 import { Weatherservice } from "./weather.service.js";
 import { Weathercache } from "./weather.cache.js";
 import { Fetchweather } from "./weather.api.js";
+import { currentweather } from "./weather.schema.js";
 
 const Fetch = new Fetchweather();
 const Cache = new Weathercache();
@@ -13,7 +14,7 @@ export const getCurrentWeather = async (req: Request, res: Response, next: NextF
   const city = req.query.city?.toString().toLowerCase();
 
   if (city === undefined) {
-    responseHandler("Please input a city", res, 400);
+    return responseHandler("Please input a city", res, 400);
   }
 
   try {
@@ -51,7 +52,7 @@ export const getWeatherForecast = async (req: Request, res: Response, next: Next
   const city = req.query.city?.toString().toLowerCase();
 
   if (city === undefined) {
-    responseHandler("Please input a city", res, 400);
+    return responseHandler("Please input a city", res, 400);
   }
 
   try {
@@ -80,6 +81,90 @@ export const getWeatherForecast = async (req: Request, res: Response, next: Next
     }
 
     return responseHandler(`Could not find the Weather Forecast for city: ${city}`, res, 404);
+  } catch (err) {
+    next(err);
+  }
+};
+
+/* DASHBOARD CONTROLLERS */
+export const getVisitorDashboard = async (req: Request, res: Response, next: NextFunction) => {
+  return res.render("home");
+};
+
+export const getClientCurrentWeather = async (req: Request, res: Response, next: NextFunction) => {
+  const city = req.query.city?.toString().toLowerCase();
+
+  if (city === undefined) {
+    // responseHandler("Please input a city", res, 400);
+    return res.status(400).redirect(`/home?message=Please+input+a+city`);
+  }
+
+  try {
+    // Check cache
+    console.log("CHECKING CACHE");
+    const a_current_weather = await Cache.get(`get:currentweather:${city}`);
+    if (a_current_weather) {
+      console.log(`FOUND SOMETHING IN CACHE`);
+      return res.render("weather.html", { req, currentweather: a_current_weather });
+    }
+
+    // Check db
+    console.log("CHECKING DATABASE");
+    const b_current_weather = await Weather.getCurrentWeather(city!);
+    if (b_current_weather) {
+      console.log(`FOUND SOMETHING IN DATABASE`);
+      return res.render("weather.html", { req, currentweather: b_current_weather });
+    }
+
+    // fetch from api
+    console.log("CHECKING API");
+    const c_current_weather = await Fetch.fetchCurrentWeather(city!);
+    if (c_current_weather) {
+      console.log(`FOUND SOMETHING IN API`);
+      return res.render("weather.html", { req, currentweather: c_current_weather });
+    }
+
+    return res
+      .status(404)
+      .redirect(`/home?message=Could+not+find+the+Current+Weather+of+city:+${city}`);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getClientWeatherForecast = async (req: Request, res: Response, next: NextFunction) => {
+  const city = req.query.city?.toString().toLowerCase();
+
+  if (city === undefined) {
+    return res.status(400).redirect(`/home?message=Please+input+a+city`);
+  }
+
+  try {
+    // Check cache
+    console.log("CHECKING CACHE");
+    const a_forecast = await Cache.get(`get:forecast:${city}`);
+    if (a_forecast) {
+      console.log(`FOUND SOMETHING IN CACHE`);
+      return res.render("weather.html", { req, forecast: a_forecast });
+    }
+
+    // Check db
+    console.log("CHECKING DATABASE");
+    const b_forecast = await Weather.getForecast(city!);
+    if (b_forecast) {
+      console.log(`FOUND SOMETHING IN DATABASE`);
+      return res.render("weather.html", { req, forecast: b_forecast });
+    }
+
+    // fetch from api
+    console.log("CHECKING API");
+    const c_forecast = await Fetch.fetchForecast(city!);
+    if (c_forecast) {
+      console.log(`FOUND SOMETHING IN API`);
+      return res.render("weather.html", { req, forecast: c_forecast });
+    }
+
+    return res.status(404).redirect(`/home?message=Could+not+find+the+Weather+Forecast+for+city:+${city}`);
   } catch (err) {
     next(err);
   }
--- src/modules/weather/weather.routes.ts
+++ src/modules/weather/weather.routes.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import { getCurrentWeather, getWeatherForecast } from "./weather.controller.js";
+import {
+  getCurrentWeather,
+  getWeatherForecast,
+  getClientCurrentWeather,
+  getClientWeatherForecast,
+} from "./weather.controller.js";
 
 const router = express.Router();
 
 // API routes
 router.get("/current", getCurrentWeather);
 router.get("/forecast", getWeatherForecast);
+
+// Dashboard routes
+router.get("/home", getClientCurrentWeather)
+router.get("/client/current", getClientWeatherForecast);
+router.get("/client/forecast", getClientWeatherForecast);
 
 export default router;
