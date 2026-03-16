3ed28823749e87ed6b8ca9c4dd85019097cfb844
Maxmillian <maximilianogbuabor@gmail.com>
2026-03-16 11:04:56 +0100

    style: added colors for logger output

--- src/configs/logger.config.ts
+++ src/configs/logger.config.ts
@@ -12,7 +12,7 @@ dotenv.config({
 const logtail = new Logtail(process.env.SOURCE_TOKEN!.toString(), {
   endpoint: `https://${process.env.INGESTING_HOST!.toString()}`,
 });
-const { combine, timestamp, json, errors, align } = winston.format;
+const { combine, timestamp, json, errors, align, cli } = winston.format;
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || "info",
@@ -23,6 +23,7 @@ const logger = winston.createLogger({
     json(),
     errors({ stack: true }),
     align(),
+    cli(),
   ),
   transports: [new winston.transports.Console(), new LogtailTransport(logtail)],
 });
